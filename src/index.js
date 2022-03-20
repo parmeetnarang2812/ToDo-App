a7252e5d095d1597b452becf1e0ca25eff2bed1d
parmeetnarang2812 <parmeetnarang28@gmail.com>
2022-03-20 21:19:24 +0530

    -

--- src/index.js
+++ src/index.js
@@ -1,127 +1,181 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import './index.css'
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
 
 class AddTask extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            taskDesc: ''
-        }
-    }
-    handleTaskTextChange(e){
-        this.setState({
-            taskDesc: e.target.value
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskDesc: "",
+    };
+  }
+  handleTaskTextChange(e) {
+    this.setState({
+      taskDesc: e.target.value,
+    });
+  }
 
-    handleAddTaskClick(){
-        this.props.handlerToCollectTaskInfo(this.state.taskDesc);
-        this.setState({
-            taskDesc: ''
-        })
-    }
+  handleAddTaskClick() {
+    this.props.handlerToCollectTaskInfo(this.state.taskDesc);
+    this.setState({
+      taskDesc: "",
+    });
+  }
 
-    render(){
-        return(
-            <form>
-                <input type="text" value={this.state.taskDesc} onChange={(e) => this.handleTaskTextChange(e)} />
-                <input type="button" value="Add Task" onClick={() => this.handleAddTaskClick()}/>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.taskDesc}
+          onChange={(e) => this.handleTaskTextChange(e)}
+        />
+        <input
+          type="button"
+          value="Add Task"
+          onClick={() => this.handleAddTaskClick()}
+        />
+      </form>
+    );
+  }
 }
 
 class TaskList extends React.Component {
-   render(){
-        let list = [];
+  handleTaskClick(taskDesc) {
+    this.props.handlerToCollectTaskClickInfo(taskDesc);
+  }
 
-        for(let i = 0; i < this.props.tasks.length; i++){
-            let task = this.props.tasks[i];
-            let spanAction;
+  render() {
+    let list = [];
 
-            if(task.isFinished){
-                spanAction = (
-                    <span class="material-icons">close</span>
-                );
-            } else {
-                spanAction = (
-                    <span class="material-icons">done</span>
-                );
-            }
+    for (let i = 0; i < this.props.tasks.length; i++) {
+      let task = this.props.tasks[i];
+      let spanAction;
 
-            let listItem = (
-                <div key={i}>
-                    <span>{task.desc}</span>
-                    {spanAction}
-                </div>
-            );
-            list.push(listItem);
-        }
+      if (task.isFinished) {
+        spanAction = (
+          <span
+            class="material-icons"
+            onClick={() => this.handleTaskClick(task.desc)}
+          >
+            close
+          </span>
+        );
+      } else {
+        spanAction = (
+          <span
+            class="material-icons"
+            onClick={() => this.handleTaskClick(task.desc)}
+          >
+            done
+          </span>
+        );
+      }
 
-        return(
-            <div className={this.props.forStyling}>
-                <div className="list-container">
-                    <div className='title'>{this.props.purpose}</div>
-                    <div className='content'>
-                        {list}
-                    </div>
-                </div>
-            </div>
-        )
+      let listItem = (
+        <div key={i}>
+          <span>{task.desc}</span>
+          {spanAction}
+        </div>
+      );
+      list.push(listItem);
     }
+
+    return (
+      <div className={this.props.forStyling}>
+        <div className="list-container">
+          <div className="title">{this.props.purpose}</div>
+          <div className="content">{list}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            tasks: [{
-                desc: 'Switch off light',
-                isFinished: false
-            }, {
-                desc: 'Turn on fan',
-                isFinished: false
-            }, {
-                desc: 'Make tea',
-                isFinished: true
-            }, {
-                desc: 'Make dinner',
-                isFinished: true
-            }]
-        }
-    }
+    this.state = {
+      tasks: [
+        {
+          desc: "Switch off light",
+          isFinished: false,
+        },
+        {
+          desc: "Turn on fan",
+          isFinished: false,
+        },
+        {
+          desc: "Make tea",
+          isFinished: true,
+        },
+        {
+          desc: "Make dinner",
+          isFinished: true,
+        },
+      ],
+    };
+  }
 
-    handleNewTask(taskDesc){
-        let oldTasks = this.state.tasks.slice();
+  handleNewTask(taskDesc) {
+    let oldTasks = this.state.tasks.slice();
 
-        oldTasks.push({
-            desc: taskDesc,
-            isFinished: false
-        });
-        this.setState({
-            tasks: oldTasks
-        })
-    }
+    oldTasks.push({
+      desc: taskDesc,
+      isFinished: false,
+    });
+    this.setState({
+      tasks: oldTasks,
+    });
+  }
 
-    render(){
-       let tasks = this.state.tasks;
-       let todoTasks = tasks.filter(t => t.isFinished == false);
-       let doneTasks = tasks.filter(t => t.isFinished == true);
+  handleTaskStatusUpdate(taskDesc, newStatus) {
+    let oldTasks = this.state.tasks.slice();
 
-       return (
-           <>
-              <div className="add-task">
-                <AddTask handlerToCollectTaskInfo={(taskDesc) => this.handleNewTask(taskDesc)}/>
-              </div>
-              <div className='task-lists'>
-                <TaskList tasks={todoTasks} purpose="Todo" forStyling="todo"/>
-                <TaskList tasks={doneTasks} purpose="Finished" forStyling="finished"/>
-              </div>
-           </>
-       );
-    }
+    let taskItem = oldTasks.find((ot) => ot.desc == taskDesc);
+    taskItem.isFinished = newStatus;
+
+    this.setState({
+      tasks: oldTasks,
+    });
+  }
+
+  render() {
+    let tasks = this.state.tasks;
+    let todoTasks = tasks.filter((t) => t.isFinished == false);
+    let doneTasks = tasks.filter((t) => t.isFinished == true);
+
+    return (
+      <>
+        <div className="add-task">
+          <AddTask
+            handlerToCollectTaskInfo={(taskDesc) =>
+              this.handleNewTask(taskDesc)
+            }
+          />
+        </div>
+        <div className="task-lists">
+          <TaskList
+            handlerToCollectTaskClickInfo={(taskDesc) =>
+              this.handleTaskStatusUpdate(taskDesc, true)
+            }
+            tasks={todoTasks}
+            purpose="Todo"
+            forStyling="todo"
+          />
+          <TaskList
+            handlerToCollectTaskClickInfo={(taskDesc) =>
+              this.handleTaskStatusUpdate(taskDesc, false)
+            }
+            tasks={doneTasks}
+            purpose="Finished"
+            forStyling="finished"
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
